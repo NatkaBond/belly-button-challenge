e2cc6236ccd116a353b638fd0079adde64c39006
NatkaBond <familybonko@gmail.com>
2023-09-21 11:40:34 -0400

    updated files

--- StarterCode-2/static/js/app.js
+++ StarterCode-2/static/js/app.js
@@ -1,142 +1,116 @@
-var option = "";
-var dataSet ;
+// // Set up URL for data
+const url =
+    "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+// Promise pending for obtaining JSON data from URL
+const dataPromise = d3.json(url);
+    console.log("Data Promise: ", dataPromise);
 
-function init() {
-
-  d3.json("samples.json").then(function(data){
-    dataSet = data;
-
-    console.log(dataSet);
-    
-    displayMetaData(940,dataSet);
-    displayHBarChart(940,dataSet);
-    displayBubbleChart(940,dataSet);
-
-    var optionMenu = d3.select("#selDataset");
-
-    data.names.forEach(function(name){
-      optionMenu.append("option").text(name);
+// // Fetch the JSON data and console log it
+d3.json(url).then(function(data){
+    console.log(data);
+});
+// Set up variables and get data from JSON for charts 
+var samples;
+var meta_data;
+d3.json(url).then(function (data) {
+    let selector = d3.select("#selDataset");
+    meta_data = data.metadata;
+    samples = data.samples;
+    data.names.forEach((id) => {
+        selector.append("option").text(id).property("value", id);
     });
- })
-}
-
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
+    metaData(meta_data[0]);
+    hbarChart(samples[0]);
+    bubbleChart(samples[0]);
+});
 
 function optionChanged(value) {
-    option = value;
-    displayMetaData(option,dataSet);
-    displayHBarChart(option,dataSet);
-    displayBubbleChart(option,dataSet);
+    const selectedId = samples.find((item) => item.id === value);
+    const demographicInfo = meta_data.find((item) => item.id == value);
+
+    // Insterting Demographic Data
+    metaData(demographicInfo);
+
+    // Bar Chart
+    hbarChart(selectedId);
+
+    // Bubble Chart
+    bubbleChart(selectedId);
+
 }
 
-function displayMetaData(option,dataSet) {
-    
-    
-    var mtdata = dataSet.metadata.filter(row => row.id == option);
-    d3.select("#sample-metadata").html(displayObject(mtdata[0]));
-        
+function metaData(demographicInfo) {
+    let demoSelect = d3.select("#sample-metadata");
+
+    demoSelect.html(
+        `id: ${demographicInfo.id} <br> 
+      ethnicity: ${demographicInfo.ethnicity} <br>
+    gender: ${demographicInfo.gender} <br>
+    age: ${demographicInfo.age} <br>
+    location: ${demographicInfo.location} <br>
+    bbtype: ${demographicInfo.bbtype} <br>
+    wfreq: ${demographicInfo.wfreq}`
+    );
 }
 
-function displayObject(obj) {
-    var str = "";
-    Object.entries(obj).forEach(([key,value]) => {
-        str += `<br>${key}:${value}</br>`;
-        if(key=="wfreq"){
-            buildGauge(value);
-            console.log("gauge value is:" +value);
-        }
-        
-    });
-    return str;
-}
+function hbarChart(selectedId) {
+    let x_axis = selectedId.sample_values.slice(0, 10).reverse();
+    let y_axis = selectedId.otu_ids
+        .slice(0, 10)
+        .reverse()
+        .map((item) => `OTU ${item}`);
+    let text = selectedId.otu_labels.slice(0, 10).reverse();
 
-function displayHBarChart(option,dataSet) {
-    
-    var barData = dataSet.samples.filter(sample => sample.id == option);
-    console.log(barData);
-    
-
-    var y = barData.map(row =>row.otu_ids);  
-    var y1 =[];
-
-    
-    for(i=0;i<y[0].length;i++){
-        y1.push(`OTU ${y[0][i]}`);
-    }
-
-    var x = barData.map(row =>(row.sample_values));
-    var text = barData.map(row =>row.otu_labels);
-    
-
-    var trace = {
-        x:x[0].slice(0,10),
-        y:y1.slice(0,10),
-        text:text[0].slice(0,10),
-        type:"bar",
-        orientation:"h",
-        
+    barChart = {
+        x: x_axis,
+        y: y_axis,
+        text: text,
+        type: "bar",
+        orientation: "h",
     };
 
-    var data = [trace];
+    let chart = [barChart];
 
-    var layout = {
-        yaxis: {
-            autorange: "reversed" 
-        }
-    }
+    let layout = {
+        margin: {
+            l: 100,
+            r: 100,
+            t: 0,
+            b: 100,
+        },
+        height: 500,
+        width: 600,
+    };
 
-    
-
-    
-    Plotly.newPlot("bar",data,layout);
+    Plotly.newPlot("bar", chart, layout);
 }
 
-function displayBubbleChart(option,dataSet) {
+function bubbleChart(selectedId) {
+    let x_axis = selectedId.otu_ids;
+    let y_axis = selectedId.sample_values;
+    let marker_size = selectedId.sample_values;
+    let color = selectedId.otu_ids;
+    let text = selectedId.otu_labels;
 
-    var barData = dataSet.samples.filter(sample => sample.id == option);
-    console.log(barData); 
-
-    var x = barData.map(row =>row.otu_ids); 
-    var y = barData.map(row =>row.sample_values); 
-    var text = barData.map(row =>row.otu_labels);
-    var marker_size = barData.map(row =>row.sample_values);
-    var marker_color = barData.map(row =>row.otu_ids);
-    
-    console.log(x[0]);
-    console.log(y[0]);
-    console.log(text);
-    
-    var trace1 = {
-        x:x[0],
-        y:y[0],
-        text: text[0],
-        mode:"markers",
+    bubble = {
+        x: x_axis,
+        y: y_axis,
+        text: text,
+        mode: "markers",
         marker: {
-            color: marker_color[0],
-            size: marker_size[0],
-            colorscale: "Earth"
-        }
-        
+            color: color,
+            colorscale: "Earth",
+            size: marker_size,
+        },
+        type: "scatter",
     };
+    let chart = [bubble];
 
-    var data = [trace1];
-
-    var layout = {
-        xaxis:{
-            title: "OTU ID"
-        }
-
+    let layout = {
+        xaxis: {
+            title: { text: "OTU ID" },
+        },
     };
-
-    Plotly.newPlot("bubble",data,layout);
-
+    Plotly.newPlot("bubble", chart, layout);
 }
-
-
-
-init();
